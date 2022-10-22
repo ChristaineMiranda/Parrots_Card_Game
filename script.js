e9a921dc0a7fa0ddd2bdffe1d831d4967957ecf8
Christaine Miranda <christaine.miranda@gmail.com>
2022-10-22 16:58:16 -0300

    versão final completa, sem o bônus

--- script.js
+++ script.js
@@ -25,11 +25,12 @@ console.log(vetorImagensAUsar);
 const areaCartas = document.querySelector('.campoCartas')//objeto para manipular área das cartas
 for(let i=0; i<quantidadeCartas; i++){
     areaCartas.innerHTML = areaCartas.innerHTML + `<div class="carta" onclick="virarCarta(this)">
-    <div class=" face frente"><img src="${vetorImagensAUsar[i]}"/></div>
-    <div class="face verso"><img src="back.png"/></div>
+    <div class=" face verso"><img src="${vetorImagensAUsar[i]}"/></div>
+    <div class="face frente"><img src="back.png"/></div>
 </div>`
 }//CAMPO CARTAS PREENCHIDO
 
+//VIRA CARTA SELECIONADA E QUANDO A SEGUNDA CARTA É CLICADA, CHAMA COMPARAÇÃO
 function virarCarta(cartaClicada){
     jogadas++;
     cartaClicada.classList.add("virada");
@@ -40,6 +41,8 @@ function virarCarta(cartaClicada){
         contador = 0;        
     }
 }
+
+//COMPARA UM PAR DE CARTAS
 function compara(){
     const cartasviradas = document.querySelectorAll('.aVerificar');
     const cartaA = cartasviradas[0].innerHTML;
@@ -48,23 +51,19 @@ function compara(){
         for(let i=0; i<cartasviradas.length; i++){
             cartasviradas[i].classList.add('acerto');
             cartasviradas[i].classList.remove('aVerificar');
-            cartasviradas[i].setAttribute('onclick', '');
-            //cartadavez.onclick = "alert(oi)";
-            console.log("guardado em cartas viradas" +cartasviradas[i])            
+            cartasviradas[i].setAttribute('onclick', '');       
         }
         parescertos = parescertos +2;
-        console.log(parescertos);
         if(parescertos === quantidadeCartas){
             alert(`Você ganhou em ${jogadas} jogadas!`);
         }
-
     }
     else{
         setTimeout(desvirar, 1000);
     }
 
 }
-
+//DESVIRA PAR SE CARTAS DIFERENTES
 function desvirar (){
     const cartasviradas = document.querySelectorAll('.aVerificar');
     for(let i=0; i<cartasviradas.length;i++){
@@ -73,57 +72,3 @@ function desvirar (){
     }
     
 }
-/*function desvirar(){
-    const viradas = document.querySelectorAll(".virada");
-    console.log(viradas);
-    for(let i=0; i<viradas.length;i++){
-        console.log(viradas[i]);
-        viradas[i].classList.remove('virada');
-    }
-    contador = 0;
-}
-
-
-/*function desvirarCarta(){
-    let virada = document.querySelector('.verso.aVerificar');
-    virada.classList.remove('verso');
-    virada = document.querySelector('.aVerificar');
-    virada.classList.remove('aVerificar');
-    //DESVIRAR CARTA QUE CONTEM AS CLASSES 'VERSO' E 'A VERIFICAR'
-}
-function compara(ultimo, atual){
-    
-    if(ultimo.innerHTML !== atual.innerHTML){
-        desvirarCarta();
-        desvirarCarta();
-    }
-    else{//se são cartas iguais
-        ultimo.classList.add('acerto');
-        ultimo.classList.remove('aVerificar');
-        atual.classList.add('acerto');
-        atual.classList.remove('aVerificar');
-    }
-}
-
-function virarCarta(cartaClicada){
-    const verificaSePrimeira = document.querySelector('.verso'); //procura se já tem carta virada
-    if(verificaSePrimeira !=null){ //executa SE JÁ TEM CARTA VIRADA
-        if(!(cartaClicada.classList.contains('verso'))){//SE O CLIQUE NÃO FOI NA MESMA CARTA, SE NÃO FOI ENTRA NO IF
-            cartaClicada.classList.add('verso');
-            cartaClicada.classList.add('aVerificar');
-
-            compara(ultimoClique, cartaClicada);        
-        }
-
-        else{ alert("Escolha outra carta");}; //SE CLIQUE FOI NA MESMA CARTA        
-    }
-    //SE NÃO TEM CARTA VIRADA - PRIMEIRA CARTA DA JOGADA
-    else{//virar a carta clicada - se primeira do par, fica virada até a segunda ser clicada
-        cartaClicada.classList.add('verso');
-        cartaClicada.classList.add('aVerificar');
-        ultimoClique = cartaClicada;
-
-    }
-               
-}*/
-    
